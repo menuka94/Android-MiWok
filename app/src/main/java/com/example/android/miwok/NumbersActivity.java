dfcc8bee9a031aaa8f45ae3b583882ccf3dbfffa
Menuka <menuka.14@cse.mrt.ac.lk>
2017-01-27 19:38:13 +0530

    Implement WordAdapter + CustomListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.WrapperListAdapter;
 
 import java.util.ArrayList;
 
@@ -13,22 +14,24 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        ArrayList<String> words = new ArrayList<>();
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
-        words.add("six");
-        words.add("seven");
-        words.add("eight");
-        words.add("nine");
-        words.add("ten");
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
+
+        WordAdapter adapter = new WordAdapter(this, words);
 
         ListView listView = (ListView) findViewById(R.id.listView);
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,words);
+        listView.setAdapter(adapter);
 
-        listView.setAdapter(itemsAdapter);
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,23 @@
+package com.example.android.miwok;
+
+/**
+ * Created by menuka on 1/27/17.
+ */
+
+public class Word {
+    private String defaultTranslation;
+    private String miwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return defaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return miwokTranslation;
+    }
+}
