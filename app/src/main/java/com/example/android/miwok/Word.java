2820478346b53a6a14867fdc8c778ee54d2b9a8f
Menuka <menuka.14@cse.mrt.ac.lk>
2017-01-30 11:15:48 +0530

    Add mp3 source files

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,17 +8,20 @@ public class Word {
     private String defaultTranslation;
     private String miwokTranslation;
     private int imageResourceId = NO_IMAGE_ID;
+    private int audioResourceId;
     private static final int NO_IMAGE_ID = -1;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
+        this.audioResourceId = audioResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
         this.imageResourceId = imageResourceId;
+        this.audioResourceId = audioResourceId;
     }
 
     /*
@@ -38,5 +41,9 @@ public class Word {
 
     public int getImageResourceId() {
         return imageResourceId;
+    }
+
+    public int getAudioResourceId() {
+        return audioResourceId;
     }
 }
